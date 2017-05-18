8afca45e1d325e8d2f14748cd4b4ed7c39388d41
ri-co <ri-co@users.noreply.github.com>
2017-05-18 17:28:57 +0200

    Added HUB & HOST - More INTERFACES can be added now in router

--- dPT_server/resources/views/home.blade.php
+++ dPT_server/resources/views/home.blade.php
@@ -44,8 +44,8 @@
           <div class="panel panel-default">
             <div class="panel-heading">
               <button type="button" onclick="routerPanel()"><img src="https://openclipart.org/download/216575/network_router_generic.svg" class="img-circle img-device-list"></button>
-
-
+              <button type="button" onclick="hubPanel()"><img src="https://openclipart.org/download/171420/switch-hub.svg" class="img-circle img-device-list"></button>
+              <button type="button" onclick="hostPanel()"><img src="https://openclipart.org/download/4714/BigRedSmile-A-new-Computer.svg" class="img-device-list"></button>
             </div>
             <div class="panel-body hidden-element" id="routerPanel">
               <form name="new-router-interface" id="interface1">
@@ -60,6 +60,28 @@
                   </fieldset>
                   <button type="button" class="btn btn-primary" onclick="insertRouter()" > Create </button>
                   <button type="button" onclick="addInterface()"> <i class="fa fa-plus" aria-hidden="true"></i> </button>
+                </form>
+            </div>
+            <div class="panel-body hidden-element" id="hubPanel">
+                <h2> Hub </h2>
+                <button type="button" class="btn btn-primary" onclick="insertHub()" > Create </button>
+            </div>
+            <div class="panel-body hidden-element" id="hostPanel">
+              <form name="new-router-interface" id="hostForm">
+                  <label class="label label-default"> New host</label>
+                  <fieldset class="form-group">
+                    <label for="host-ipaddr">IP Address</label>
+                    <input type="text" class="form-control" id="host-ipaddr" placeholder="e.g. 192.168.254.254" onkeydown="if (event.keyCode == 13) { insertRouter(); return false; }">
+                  </fieldset>
+                  <fieldset class="form-group">
+                    <label for="host-netmask">Netmask</label>
+                    <input type="text" class="form-control" id="host-netmask" placeholder="e.g. 255.255.0.0" onkeydown="if (event.keyCode == 13) { insertRouter(); return false; }">
+                  </fieldset>
+                  <fieldset class="form-group">
+                    <label for="host-dgateway">Default gateway</label>
+                    <input type="text" class="form-control" id="host-dgateway" placeholder="e.g. 192.168.254.254" onkeydown="if (event.keyCode == 13) { insertRouter(); return false; }">
+                  </fieldset>
+                  <button type="button" class="btn btn-primary" onclick="insertHost()" > Create </button>
                 </form>
             </div>
           </div>
@@ -88,6 +110,23 @@ if(pname != "") {
   console.log("Valore Null");
 }
 
+function addInterface() {
+
+  var id = 1;
+  while($("#interface"+ id).length != 0) {
+    id++;
+  }
+  var newForm = $("#interface1").clone();
+  console.log(newForm);
+  newForm[0].id = "interface"+id;
+  newForm[0].firstChild.textContent = "Interface "+id;
+  newForm[0][1].id = "interface"+id+"-ipaddr";
+  newForm[0][3].id = "interface"+id+"-netmask";
+  newForm[0][4].remove();
+  newForm[0][4].remove();
+  newForm.appendTo("#routerPanel");
+}
+
 function openPanel(pname) {
   $('#projects-home').hide();
   console.log(pname);
@@ -105,19 +144,74 @@ function routerPanel() {
 
 }
 
+function hubPanel() {
+
+  if($('#hubPanel').css('display') == 'none')
+    $('#hubPanel').show();
+  else
+    $('#hubPanel').hide();
+
+}
+
+function hostPanel() {
+
+  if($('#hostPanel').css('display') == 'none')
+    $('#hostPanel').show();
+  else
+    $('#hostPanel').hide();
+
+}
+
+function insertHub() {
+  var pname = $('#pname').text();
+  var req_data ={"devices": [{ "id": "",
+                                "project": pname,
+                                "dtype": "HUB",
+                            }]};
+  $.ajax({
+    url: '/api/projects/' + pname,
+    type: 'PUT',
+    data: req_data
+  });
+}
+
+function insertHost() {
+
+    /*prende i dati della/delle interfacce di rete -
+    da fare in loop (ciclo con contatore ripetuto n volte; n = numero interfacce) */
+    var pname = $('#pname').text();
+    var req_data ={"devices": [{ "id": "",
+                                  "project": pname,
+                                  "dtype": "HOST",
+                                  "ipaddr": $('#host-ipaddr').val(),
+                                  "netmask": $('#host-netmask').val(),
+                                  "dgateway": $('#host-dgateway').val()
+                              }]};
+    //richiesta PUT /api/projects/:id
+
+    $.ajax({
+      url: '/api/projects/' + pname,
+      type: 'PUT',
+      data: req_data
+    });
+}
+
 function insertRouter() {
 
     /*prende i dati della/delle interfacce di rete -
     da fare in loop (ciclo con contatore ripetuto n volte; n = numero interfacce) */
     var pname = $('#pname').text();
-    var ip_address = $('#interface1-ipaddr').val();
-    var netmask = $('#interface1-netmask').val();
-
     var req_data ={"devices": [{ "id": "",
                                   "project": pname,
                                   "dtype": "ROUTER",
-                                  "interfaces": [{"id": "", "ipaddr" : ip_address , "netmask" : netmask}]
+                                  "interfaces": []
                               }]};
+    for(var id = 1; $("#interface"+ id).length != 0; id++) {
+      console.log("entro")
+        var ip_address = $('#interface'+id+'-ipaddr').val();
+        var netmask = $('#interface'+id+'-netmask').val();
+        req_data.devices[0].interfaces.push({"id": "", "ipaddr" : ip_address , "netmask" : netmask});
+    }
     //richiesta PUT /api/projects/:id
 
     $.ajax({
